fc53fafd48904c1910b94aee2f27b092c99e6cdc
Aditya YV <adityayv802@gmail.com>
2025-04-08 20:47:00 -0500

    smsm

--- include/display.h
+++ include/display.h
@@ -1,15 +1,6 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include <SPI.h>
-
-#include <FS.h>
-#include <SD.h>
-
-#include <TFT_eSPI.h>
-
-// JPEG decoder library
-#include <JPEGDecoder.h>
 
 void initDisplay(bool SD_enable = true);
 void drawSdJpeg(const char *filename, int xpos, int ypos);
--- src/display.cpp
+++ src/display.cpp
@@ -1,9 +1,19 @@
 #include "display.h"
 
+#include <SPI.h>
+
+#include <FS.h>
+#include <SD.h>
+
+#include <TFT_eSPI.h>
+
+// JPEG decoder library
+#include <JPEGDecoder.h>
+
 TFT_eSPI tft = TFT_eSPI();
 
 //Array of random colors to cycle through
-uint16_t colors[] = {TFT_RED, TFT_GREEN, TFT_BLUE, TFT_YELLOW, TFT_CYAN, TFT_MAGENTA};
+int colors[] = {TFT_RED, TFT_GREEN, TFT_BLUE, TFT_YELLOW, TFT_CYAN, TFT_MAGENTA};
 
 void rotateColors() {
   static uint8_t colorIndex = 0;
@@ -20,35 +30,33 @@ void initDisplay(bool SD_enable){
     tft.fillScreen(TFT_BLACK);
 
     tft.begin();
-    if (!SD.begin(5, tft.getSPIinstance()) && SD_enable) {
-        Serial.println("Card Mount Failed");
-        return;
+    
+    if (!SD.begin(5, tft.getSPIinstance())) {
+      Serial.println("Card Mount Failed");
+      return;
     }
-    if (SD_enable){
-        uint8_t cardType = SD.cardType();
-
-        if (cardType == CARD_NONE) {
-            Serial.println("No SD card attached");
-            return;
-        }
-
-        Serial.print("SD Card Type: ");
-        if (cardType == CARD_MMC) {
-            Serial.println("MMC");
-        } else if (cardType == CARD_SD) {
-            Serial.println("SDSC");
-        } else if (cardType == CARD_SDHC) {
-            Serial.println("SDHC");
-        } else {
-            Serial.println("UNKNOWN");
-        }
-
-        uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-        Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-        Serial.println("initialisation done.");
+    uint8_t cardType = SD.cardType();
+  
+    if (cardType == CARD_NONE) {
+      Serial.println("No SD card attached");
+      return;
     }
-
+  
+    Serial.print("SD Card Type: ");
+    if (cardType == CARD_MMC) {
+      Serial.println("MMC");
+    } else if (cardType == CARD_SD) {
+      Serial.println("SDSC");
+    } else if (cardType == CARD_SDHC) {
+      Serial.println("SDHC");
+    } else {
+      Serial.println("UNKNOWN");
+    }
+  
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+  
+    Serial.println("initialisation done.");
 }
 
 void drawSdJpeg(const char *filename, int xpos, int ypos, bool centered = false) {
